8de5f830c5df5e19d633d1a57fd08453198447f2
Monica Nadal <missmonicanadal@gmail.com>
2020-07-26 15:35:53 -0700

    fixed onclick and state sync up and fixed score uptick

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // import React from 'react';
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
 // import Game from './components/Game';
 import GameWrapper from './components/GameWrapper';
 import cards from '../src/CardArray.json';
@@ -10,24 +10,62 @@ import Card from './components/Card';
 
 class App extends Component {
   state = {
-    cards
+    cards: cards,
+    score: 0,
+    topScore: 0
   };
+
+  handleClick = (id) => {
+    var cards = this.state.cards
+    var score = this.state.score
+    var topScore = this.state.topScore
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].id === id) {
+        if (cards[i].click === false) {
+          cards[i].click= true;
+          score++;
+          var cardsShuff = this.shuffleData(cards)
+        }
+        // else {
+        //   topScore = this.state.topScore
+        //   if (score > topScore) {
+        //     this.setState({ topScore: score, score: 0, cards: cards })
+        //     //if return doesn't work ... we need to stop the for loop some other way
+        //     //if this doesn't work, change the for loop to for while
+        //     return
+        //   }
+        // }
+      }
+    }
+    this.setState({ cards: cardsShuff, score: score })
+  }
+
+  shuffleData = data => {
+    let i = data.length - 1;
+    while (i > 0) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = data[i];
+      data[i] = data[j];
+      data[j] = temp;
+      i--;
+    }
+    return data;
+  };
+
 
   render() {
     return (
-      <>
-        <Header />
-        {/* <Game /> */}
-        <GameWrapper> {this.state.cards.map(card => (
-            <Card 
+      <GameWrapper>
+        {this.state.cards.map(card => (
+          <Card
             id={card.id}
             key={card.id}
             image={card.image}
-            />
-          ))}
-        </GameWrapper>
-        <Footer />
-      </>
+            name={card.name}
+            handleClick={this.handleClick}
+          />
+        ))}
+      </GameWrapper>
     );
   }
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,11 @@
 import React from 'react';
-// import CardArray from '../components/CardArray.json'
-// import dataCard from '../CardArray.json'
 
 function Card(props) {
     return (
-        // <>
-        // <p>This is the card component.</p>
-        // <div className='card'>
-        //     <div className='img-container'>
-        //         <img alt='some text' src={props.image}></img>
-        //     </div>
-        // </div>
-        // </>
-        <div className='card'>
-            <button className='img-container'>
-                <img alt={props.id} src={props.image} />
-            </button>
+        <button className='card' id={props.id} onClick={() => props.handleClick(props.id)}>
+            <div className='img-container'>
+                <img alt={props.id} src={props.image}/>
+            </div>
             {/* <div className='content'>
                 <ul>
                     <li>Card Id Test: {props.id}</li>
@@ -24,7 +14,7 @@ function Card(props) {
             {/* <button>Click here</button> */}
             {/* <button onClick={props.handleClick(props.card.id)}><img src={props.card.image}></img></button> */}
             {/* <img src={props.card.image} onClick={props.handleClick(props.card.id)}></img> */}
-        </div>
+        </button>
     )
 }
 
