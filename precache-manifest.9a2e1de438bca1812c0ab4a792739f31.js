40362b7cf9adb9ad52052008940a9c882f8f5f60
Monica Nadal <missmonicanadal@gmail.com>
2020-07-27 13:02:02 -0700

    Updates

---
+++ precache-manifest.9a2e1de438bca1812c0ab4a792739f31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f2ddf0cd04c4cf3c7da34924d24bc5",
+    "url": "/clickgame/index.html"
+  },
+  {
+    "revision": "39788716d176cb5f1f35",
+    "url": "/clickgame/static/css/main.5cf74b1d.chunk.css"
+  },
+  {
+    "revision": "29d40325a51744729faa",
+    "url": "/clickgame/static/js/2.d8ee25d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickgame/static/js/2.d8ee25d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39788716d176cb5f1f35",
+    "url": "/clickgame/static/js/main.41032a42.chunk.js"
+  },
+  {
+    "revision": "818b3e64b8c78b391a97",
+    "url": "/clickgame/static/js/runtime-main.7de684e1.js"
+  }
+]);
